52095b2753a17d92425be4850f0ae05f8cd8519c
FarisHrvat <80819846+FarisHrvat@users.noreply.github.com>
2023-02-10 11:28:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,30 @@
-import flask
-from flask import Flask, request, render_template
-import datetime
-import mysql.connector
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
-app = Flask(__name__)
 
-# Connect to Google Cloud SQL database
-mydb = mysql.connector.connect(
-  host="host_name",
-  user="user_name",
-  password="password",
-  database="database_name"
-)
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCiIonF87F8yOS0YmPeXl3ZdNYQrVPvDR0",
+  authDomain: "labaratorynotebook.firebaseapp.com",
+  projectId: "labaratorynotebook",
+  storageBucket: "labaratorynotebook.appspot.com",
+  messagingSenderId: "917925815807",
+  appId: "1:917925815807:web:e6f599480e7f5385a773ca",
+  measurementId: "G-HE3GF7LJ85"
+};
 
-# Create table for storing experiment entries
-mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE IF NOT EXISTS experiments (id INT AUTO_INCREMENT PRIMARY KEY, date DATE, title VARCHAR(255), entry TEXT)")
+def add_entry(date, experiment, results, observations):
+    ref = db.reference('entries')
+    entry = {
+        "date": date,
+        "experiment": experiment,
+        "results": results,
+        "observations": observations
+    }
+    ref.push(entry)
 
-# Route for main page that displays table of contents
-@app.route('/')
-def index():
-    mycursor.execute("SELECT id, date, title FROM experiments")
-    experiments = mycursor.fetchall()
-    return render_template('index.html', experiments=experiments)
-
-# Route for adding new experiment entry
-@app.route('/add', methods=['GET', 'POST'])
-def add_entry():
-    if request.method == 'POST':
-        title = request.form['title']
-        entry = request.form['entry']
-        date = datetime.datetime.now().strftime("%Y-%m-%d")
-        mycursor.execute("INSERT INTO experiments (date, title, entry) VALUES (%s, %s, %s)", (date, title, entry))
-        mydb.commit()
-        return flask.redirect('/')
-    return render_template('add.html')
-
-# Route for viewing individual experiment entry
-@app.route('/view/<int:id>')
-def view_entry(id):
-    mycursor.execute("SELECT * FROM experiments WHERE id = %s", (id,))
-    experiment = mycursor.fetchone()
-    return render_template('view.html', experiment=experiment)
-
-if __name__ == '__main__':
-    app.run()
+def view_entries():
+    ref = db.reference('entries')
+    entries = ref.get()
+    return entries
